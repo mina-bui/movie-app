608b08980b27f7b399c8f07d24c25fc1733be190
Mina Bui <mbui14@my.bcit.ca>
2021-01-11 18:29:40 -0800

    starting off html for about.js, singlemovie.js

--- react-app-starter/src/components/SingleMovie.js
+++ react-app-starter/src/components/SingleMovie.js
@@ -4,7 +4,31 @@ function SingleMovie() {
   return (
     <main>
       <section>
-          <h2>Single Movie Page</h2>
+        <h2>Single Movie Page</h2>
+        <section>
+          <p>
+            Release Date:
+            </p>
+          <p>
+            Release Date:
+            </p>
+          <p>
+            Release Date:
+            </p>
+          <span>Rating:</span>
+          <div>81%</div>
+        </section>
+        <section>
+          <h3>Overview</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </section>
       </section>
     </main>
   );

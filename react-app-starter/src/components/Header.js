1079a3c7d08f8f15cf1fbc230d0f899ed7dcbf61
v-nguyen6 <73795871+v-nguyen6@users.noreply.github.com>
2021-01-13 18:08:47 -0800

    Update Header.js

--- react-app-starter/src/components/Header.js
+++ react-app-starter/src/components/Header.js
@@ -3,12 +3,26 @@ import { Link } from 'react-router-dom';
 import Nav from './Nav';
 
 function Header() {
-  return (
-    <header>
-      <h1><Link to={'/'}>Cinedex</Link></h1>
-      <Nav />
-    </header>
-  );
+	return (
+		<header>
+			<div className="header-row">
+				<h1><Link to={'/'}>Cinedex</Link></h1>
+				<button className="btn-main-nav">
+					<span className="hamburger-icon">
+						<span className="line"></span>
+						<span className="line"></span>
+						<span className="line"></span>
+					</span>
+					<span className="sr-only">Menu</span>
+				</button>
+				<div className="search-bar"></div>
+			</div>
+
+			<div className="header-row">
+				<Nav />
+			</div>
+    	</header>
+  	);
 }
 
 export default Header;

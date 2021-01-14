b151baa60198b37a724b9fff04deef42191881a7
Mina Bui <mbui14@my.bcit.ca>
2021-01-13 18:04:15 -0800

    about css styling

--- react-app-starter/src/components/About.js
+++ react-app-starter/src/components/About.js
@@ -2,27 +2,24 @@
 
 function About() {
   return (
-    <main>
-      <section>
-        <h2>About MoviesGo</h2>
-        <p>
-          MoviesGo is a movie database site hoping to satisfy the cinema needs of movie
-          buffs. Whether you are planning to watch a new upcoming release, or want to
-          learn more about solid block-buster classics, MoviesGo aims to provide a
-          beautiful and seamless experience for all.
-        </p>
-        <p>
-          Please feel free to browse through our selections. If there are any movies that
-          interest you, go ahead and add them to your Favorites.
-        </p>
-        <p>
-          <i>
-            This product usese the TMDb API but is not endorsed or certified by TMDb.
-            <br></br>
-            For educational purposes only.
-          </i>
-        </p>
-      </section>
+    <main class="about-main">
+      <a href="#0" class="go-back">&#5176; Back</a>
+      <h2>About MoviesGo</h2>
+      <p>
+        MoviesGo is a movie database site hoping to satisfy the cinema needs of movie
+        buffs. Whether you are planning to watch a new upcoming release, or want to
+        learn more about solid block-buster classics, MoviesGo aims to provide a
+        beautiful and seamless experience for all.
+      </p>
+      <p>
+        Please feel free to browse through our selections. If there are any movies that
+        interest you, go ahead and add them to your Favorites.
+      </p>
+      <p>
+          This product uses the TMDb API but is not endorsed or certified by TMDb.
+          <br></br>
+          For educational purposes only.
+      </p>
     </main>
   );
 }

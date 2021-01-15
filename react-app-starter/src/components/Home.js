54c1bea22f329da31c74931597e54bb48031ecf9
Mina Bui <mbui14@my.bcit.ca>
2021-01-14 22:28:43 -0800

    basic home grid css done

--- react-app-starter/src/components/Home.js
+++ react-app-starter/src/components/Home.js
@@ -6,127 +6,184 @@ function Home() {
 			<section>
 				<div className="desktop-movie-grid">
 					<div className="movies movie-01">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa"/>
-						</figure>
-
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-02">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
-						</figure>
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-03">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
-						</figure>
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-04">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa"/>
-						</figure>
-
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-05">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
-						</figure>
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-06">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
-						</figure>
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-07">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa"/>
-						</figure>
-
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-08">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
-						</figure>
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-09">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
-						</figure>
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-10">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa"/>
-						</figure>
-
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-11">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
-						</figure>
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 					<div className="movies movie-12">
-						<figure>
-							<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
-						</figure>
-						<div className="movie-title">The War With Grandpa</div>
-						<div className="movie-genre">Comedy, Family, Drama</div>
-						<div className="movie-date">[10/09/2020]</div>
-						<div className="movie-runtime">1H, 34M</div>
+						<div className="movie-container">
+							<figure class="movie-poster">
+								<a href="#0">
+									<img src="/assets/images/the-war-with-grandpa.jpg" alt="The War With Grandpa" />
+								</a>
+							</figure>
+							<div className="movie-title"><a href="#0">The War With Grandpa</a></div>
+							<div className="movie-genre">Comedy, Family, Drama</div>
+							<div className="movie-date">[10/09/2020]</div>
+							<div className="movie-rating">81%</div>
+							<div className="movie-runtime">1H, 34M</div>
+						</div>
 					</div>
 
 				</div>

d1f312d6e1311425306262441f15c64d8600ca49
Mina Bui <mbui14@my.bcit.ca>
2021-01-14 21:20:09 -0800

    favourites page (no movies added) done

--- react-app-starter/src/components/Favourites.js
+++ react-app-starter/src/components/Favourites.js
@@ -2,10 +2,35 @@
 
 function Favourites() {
   return (
-    <main>
-      <section>
-          <h2>Favourited Movies go here...</h2>
-      </section>
+    <main class="favourite-main">
+
+      <a href="#0" class="go-back">&#5176; Back</a>
+
+      <h2>Favourites</h2>
+
+      <div class="favourite-tutorial-icons">
+        <img src="../assets/images/add icon.png" class="favourite_icon"></img>
+        <img src="../assets/images/arrow_icon.png" class="favourite_icon"></img>
+        <img src="../assets/images/favourite_icon.png" class="favourite_icon"></img>
+      </div>
+
+      <p>
+        Uh-oh, looks like you haven't chosen any movies yet!
+        </p>
+      <p>
+        Feel free to return back to the home page and explore more movies!
+        </p>
+      <p>
+        To add a favourite movie on the home page, you can click on the
+          <img src="../assets/images/favourite_icon.png" class="favourite_icon"></img>
+           while hovering or by clicking on the movie poster.
+        </p>
+      <p>
+        You can also find the
+          <img src="../assets/images/favourite_icon.png" class="favourite_icon"></img>
+           when you click on an individual movie to learn more or watch a trailer.
+        </p>
+
     </main>
   );
 }

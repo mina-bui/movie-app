26b2152ffdfc5bb561bb853e52f2a6ecb37b92d4
Mina Bui <mbui14@my.bcit.ca>
2021-01-14 20:47:35 -0800

    basic singlemovie css done

--- react-app-starter/src/components/SingleMovie.js
+++ react-app-starter/src/components/SingleMovie.js
@@ -3,74 +3,56 @@
 function SingleMovie() {
   return (
     <main class="singlemovie-main">
+
       <a href="#0" class="go-back">&#5176; Back</a>
+
       <div class="grid-container">
-        <div class="item1">
+
+        <div class="title-desc">
           <h2>Memories of Murder</h2>
           <p>Release Date: 10/19/2020</p>
           <p>Crime, Drama, Thriller</p>
           <p>Runtime: 2H1 11M</p>
         </div>
-        <div class="item2">
+
+        <div class="poster">
           <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
+          <p>
+            <img src="../assets/images/play_icon.png" class="play_icon"></img>
+            <a href="#0">Watch Trailer</a>
+          </p>
         </div>
-        <div class="item3">
+
+        <div class="rating">
           <p class="singlemovie-rating">Rating: <span>81%</span></p>
         </div>
-        <div class="item4">
-          Add/Remove Favorites
+
+        <div class="fave">
+          <p>
+            <img src="../assets/images/favourite_icon.png" class="favourite_icon"></img>
+            <a href="#0">Add / Remove Favorites</a>
+          </p>
         </div>
+
         <div class="item5">
-          <h3>Overview</h3>
+          <h3><a href="#0" class="not-active">Overview</a></h3>
         </div>
+
         <div class="item6">
-          <h3>Media</h3>
+          <h3><a href="#0">Media</a></h3>
         </div>
+
         <div class="item7">
-          <h3>Related Movies</h3>
+          <h3><a href="#0">Related Movies</a></h3>
         </div>
+
         <div class="item8">
           <p>
             In 1986, Park (Song Kang-ho) and Cho (Kim Roi-ha) are two simple-minded detectives assigned to a double murder investigation in a South Korean province. But when the murderer strikes several more times with the same pattern, the detectives realize that they are chasing the country's first documented serial killer. Relying on only their basic skills and tools, Park and Jo attempt to piece together the clues and solve the case in this thriller based on true events.
           </p>
         </div>
-        <div class="item9">
-          <p>Watch Trailer</p>
-        </div>
+
       </div>
-      {/*       <section>
-        <div class="singlemovie-poster-container">
-          <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
-          <p>Watch Trailer</p>
-        </div>
-        <h2>Memories of Murder</h2>
-        <section>
-          <p>Release Date: 10/19/2020</p>
-          <p>Crime, Drama, Thriller</p>
-          <p>Runtime: 2H1 11M</p>
-          <p class="singlemovie-rating">Rating: <span>81%</span></p>
-        </section>
-        <section>
-          <h3>Overview</h3>
-          <h3 class="singlemovie-desktop-tabs">Media</h3>
-          <h3 class="singlemovie-desktop-tabs">Related Movies</h3>
-          <p>
-            In 1986, Park (Song Kang-ho) and Cho (Kim Roi-ha) are two simple-minded detectives assigned to a double murder investigation in a South Korean province. But when the murderer strikes several more times with the same pattern, the detectives realize that they are chasing the country's first documented serial killer. Relying on only their basic skills and tools, Park and Jo attempt to piece together the clues and solve the case in this thriller based on true events.
-          </p>
-          <div class="singlemovie-desktop-tabs">
-            <p class="singlemovie-desktop-tabs-img">
-              <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
-              <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
-              <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
-            </p>
-            <p class="singlemovie-desktop-tabs-img">
-              <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
-              <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
-              <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
-            </p>
-          </div>
-        </section>
-      </section> */}
     </main>
   );
 }

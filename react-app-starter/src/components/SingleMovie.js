052dd3497d26b271bed7ac9c753d5fa5404aa1a5
Mina Bui <mbui14@my.bcit.ca>
2021-01-14 19:09:14 -0800

    singlemovie grid - cont.

--- react-app-starter/src/components/SingleMovie.js
+++ react-app-starter/src/components/SingleMovie.js
@@ -5,19 +5,40 @@ function SingleMovie() {
     <main class="singlemovie-main">
       <a href="#0" class="go-back">&#5176; Back</a>
       <div class="grid-container">
-        <div class="item1">Title and Description</div>
-        <div class="item2">Poster</div>
-        <div class="item3">Rating</div>
-        <div class="item4">Add/Remove Favorites</div>
-        <div class="item5">Overview</div>
-        <div class="item6">Media</div>
-        <div class="item7">Related Movies</div>
-        <div class="item8">Overview - Extended</div>
-        <div class="item9">Media - Extended</div>
-        <div class="item10">Related Movies- Extended</div>
-        <div class="item11">Watch Trailer</div>
+        <div class="item1">
+          <h2>Memories of Murder</h2>
+          <p>Release Date: 10/19/2020</p>
+          <p>Crime, Drama, Thriller</p>
+          <p>Runtime: 2H1 11M</p>
+        </div>
+        <div class="item2">
+          <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
+        </div>
+        <div class="item3">
+          <p class="singlemovie-rating">Rating: <span>81%</span></p>
+        </div>
+        <div class="item4">
+          Add/Remove Favorites
+        </div>
+        <div class="item5">
+          <h3>Overview</h3>
+        </div>
+        <div class="item6">
+          <h3>Media</h3>
+        </div>
+        <div class="item7">
+          <h3>Related Movies</h3>
+        </div>
+        <div class="item8">
+          <p>
+            In 1986, Park (Song Kang-ho) and Cho (Kim Roi-ha) are two simple-minded detectives assigned to a double murder investigation in a South Korean province. But when the murderer strikes several more times with the same pattern, the detectives realize that they are chasing the country's first documented serial killer. Relying on only their basic skills and tools, Park and Jo attempt to piece together the clues and solve the case in this thriller based on true events.
+          </p>
+        </div>
+        <div class="item9">
+          <p>Watch Trailer</p>
+        </div>
       </div>
-{/*       <section>
+      {/*       <section>
         <div class="singlemovie-poster-container">
           <img src="https://www.insidethekraken.com/wp-content/uploads/2018/10/Memories-of-Murder-poster.jpg"></img>
           <p>Watch Trailer</p>

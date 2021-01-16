88ceb674d46d5755bfbf58dbf16a27ab9416d2ca
Mina Bui <mbui14@my.bcit.ca>
2021-01-15 23:04:08 -0800

    footer styling

--- react-app-starter/src/components/Footer.js
+++ react-app-starter/src/components/Footer.js
@@ -7,48 +7,53 @@ function Footer() {
 
   return (
     <footer>
-      <div class="contact">
-        <h3>Get in Touch</h3>
+      <div class="footer-grid">
+        <div class="contact">
+          <h3>Get in Touch</h3>
 
-      </div>
-      <div class="contact-info">
-        <h5>Email</h5>
-        <p>
-          customerservice@filmdex.ca
+        </div>
+        <div class="contact-info">
+          <h5>Email</h5>
+          <p>
+            mbui14@my.bcit.ca
             <br></br>
-            (604)-123-456
+            (604)-412-7773
           </p>
-      </div>
-      <div class="social">
-        <h5>Social Media</h5>
+        </div>
+        <div class="social">
+          <h5>Social Media</h5>
 
-        <a href="www.instagram.com">
-          <img src="/../assets/images/instagram-icon.png"></img>
-        </a>
+          <a href="www.instagram.com">
+            <img src="/../assets/images/instagram-icon.png" id="instagram-icon"></img>
+          </a>
 
-        <a href="www.facebook.com">
-          <img src="/../assets/images/facebook-icon.png"></img>
-        </a>
+          <a href="www.facebook.com">
+            <img src="/../assets/images/facebook-icon.png" id="facebook-icon"></img>
+          </a>
 
-        <a href="www.twitter.com">
-          <img src="/../assets/images/twitter-icon.png"></img>
-        </a>
+          <a href="www.twitter.com">
+            <img src="/../assets/images/twitter-icon.png" id="twitter-icon"></img>
+          </a>
 
-      </div>
-      <div class="location">
-        <h3>Where We Are</h3>
-      </div>
-      <div class="address">
-        <h5>Address</h5>
-        <p>
-            Filmdex Headquarters
-            <br></br>
+        </div>
+        <div class="location">
+          <h3>Where We Are</h3>
+        </div>
+        <div class="address">
+          <h5>Address</h5>
+          <p>
             555 Seymour Street
             <br></br>
-            Vancouver, BC V6B 3H6
+            Vancouver, BC
+            <br></br>
+            V6B 3H6
           </p>
+        </div>
+        <div class="empty"></div>
       </div>
-      <p>&copy; {year} Movie Corp.</p>
+      <div class="copyright">
+        <p>&copy; {year} Movie Corp.</p>
+      </div>
     </footer>
   );
 }

0a2a796242c3930cc9891e02a28b3f9b5c1534fc
Mina Bui <mbui14@my.bcit.ca>
2021-01-17 21:16:18 -0800

    minor changes

--- react-app-starter/src/components/Nav.js
+++ react-app-starter/src/components/Nav.js
@@ -5,21 +5,13 @@ const Nav = ({ hideNav }) => (
 
     <nav onClick={hideNav}>
         <ul>
-            <li>
-                <NavLink to={'/'}>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to={'/favourites'}>Favourites</NavLink>
-            </li>
-            <li>
-                <NavLink to={'/about'}>About</NavLink>
-            </li>
-            <li>
-                <NavLink to={'/single-movie'}>Single Movie</NavLink>
-            </li>
+            <li><NavLink to={'/'}>Home</NavLink></li>
+            <li><NavLink to={'/favourites'}>Favourites</NavLink></li>
+            <li><NavLink to={'/about'}>About</NavLink></li>
+            <li><NavLink to={'/single-movie'}>Single Movie</NavLink></li>
         </ul>
     </nav>
-  );
+);
 
 
 export default Nav;
--- react-app-starter/src/components/SingleMovie.js
+++ react-app-starter/src/components/SingleMovie.js
@@ -1,6 +1,34 @@
 // Single Movie
 
 function SingleMovie() {
+
+  /*   function toggleOverview() {
+      var OverviewInfo = document.getElementsByClassName("overview-info");
+      if (OverviewInfo.style.display === "none") {
+        OverviewInfo.style.display = "block";
+      } else {
+        OverviewInfo.style.display = "none";
+      }
+    }
+  
+    function toggleMedia() {
+      var MediaInfo = document.getElementsByClassName("media-info");
+      if (MediaInfo.style.display === "none") {
+        MediaInfo.style.display = "block";
+      } else {
+        MediaInfo.style.display = "none";
+      }
+    }
+  
+    function toggleRelated() {
+      var RelatedInfo = document.getElementsByClassName("related-info");
+      if (RelatedInfo.style.display === "none") {
+        RelatedInfo.style.display = "block";
+      } else {
+        RelatedInfo.style.display = "none";
+      }
+    } */
+
   return (
 
     <section className="single-movie-section">
@@ -25,21 +53,33 @@ function SingleMovie() {
           <p><img src="/../assets/images/favourite_icon.png" className="favourite_icon"></img><a href="#0">Add / Remove Favorites</a></p>
         </div>
 
-        <div className="overview">
+        {/*         <div className="overview" onClick={toggleOverview()}>
+ */}        <div className="overview">
           <h3><a href="#0" className="not-active">Overview</a></h3>
         </div>
 
-        <div className="media">
+        {/*         <div className="media" onClick={toggleMedia()}>
+ */}        <div className="media">
           <h3><a href="#0">Media</a></h3>
         </div>
 
-        <div className="related">
+        {/*         <div className="related" onClick={toggleRelated()}>
+ */}        <div className="related">
           <h3><a href="#0">Related Movies</a></h3>
         </div>
 
         <div className="tab-info">
-          <p>In 1986, Park (Song Kang-ho) and Cho (Kim Roi-ha) are two simple-minded detectives assigned to a double murder investigation in a South Korean province. But when the murderer strikes several more times with the same pattern, the detectives realize that they are chasing the country's first documented serial killer. Relying on only their basic skills and tools, Park and Jo attempt to piece together the clues and solve the case in this thriller based on true events.</p>
+          <div className="overview-info">
+            <p>In 1986, Park (Song Kang-ho) and Cho (Kim Roi-ha) are two simple-minded detectives assigned to a double murder investigation in a South Korean province. But when the murderer strikes several more times with the same pattern, the detectives realize that they are chasing the country's first documented serial killer. Relying on only their basic skills and tools, Park and Jo attempt to piece together the clues and solve the case in this thriller based on true events.</p>
+          </div>
+          <div className="media-info">
+            <p>Media Info Section</p>
+          </div>
+          <div className="related-info">
+            <p>Related Info Section</p>
+          </div>
         </div>
+
       </section>
     </section>
   );
